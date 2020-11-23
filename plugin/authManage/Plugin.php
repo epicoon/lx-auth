a85b60c336b58df350be2e0ca01c650250241cbf
epicoon <altkkb.vds@mail.ru>
2020-11-23 13:27:17 +0700

    refs #0049 - Косметические правки

--- plugin/authManage/Plugin.php
+++ plugin/authManage/Plugin.php
@@ -2,8 +2,19 @@
 
 namespace lx\auth\plugin\authManage;
 
-class Plugin extends \lx\Plugin {
-	public function init() {
+use lx\Plugin as lxPlugin;
+
+/**
+ * Class Plugin
+ * @package lx\auth\plugin\authManage
+ */
+class Plugin extends lxPlugin
+{
+    /**
+     * @return void
+     */
+	public function init()
+    {
 		$this->attributes->userModel = $this->app->userProcessor
 			? $this->app->userProcessor->getUserModelName()
 			: '';
